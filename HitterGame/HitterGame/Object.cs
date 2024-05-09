503c6937ebcfb6f9312c589df435dd7144760de6
unknown <kimbklove@gmail.com>
2024-05-09 12:23:00 +0900

    최신버전(버그 많음)

--- HitterGame/HitterGame/Object.cs
+++ HitterGame/HitterGame/Object.cs
@@ -19,19 +19,34 @@ namespace HitterGame
         private int jangta;
         public void DrawIntro()
         {
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine("\n 야구 게임을 시작합니다!\n");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
-            Console.WriteLine($"\n{new string(' ', 12)}*");
+            Console.SetCursorPosition(22, 1);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 3);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 5);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 7);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 9);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 11);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(11, 13);
+            Console.WriteLine("야구 게임을 시작합니다!");
+            Console.SetCursorPosition(22, 15);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(14, 17);
+            Console.WriteLine("아무키나 누르세요");
+            Console.SetCursorPosition(22, 19);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 21);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 23);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 25);
+            Console.WriteLine($"*");
+            Console.SetCursorPosition(22, 27);
+            Console.WriteLine($"*");
         }
 
         public DrawingObject(int width, int height)
@@ -45,11 +60,23 @@ namespace HitterGame
             Console.Clear();
             DrawBorder();
         }
-
         public void Draw02()
         {
             Console.Clear();
             DrawBorder02();
+        }
+        public void MainDraw()
+        {
+            Console.Clear();
+            DrawBorder02();
+            MainView();
+        }
+
+        public void manualView()
+        {
+            Console.Clear();
+            DrawBorder02();
+            MainView();
         }
 
         private void DrawBorder()
@@ -72,6 +99,52 @@ namespace HitterGame
                 Console.WriteLine($"||{new string(' ', width)}||");
             }
             Console.WriteLine(new string('=', width + 4));
+        }
+
+        private void manual()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(28, 4);
+            Console.WriteLine("._ _  _ ._     _ |");
+            Console.SetCursorPosition(28, 5);
+            Console.WriteLine("| | |(_|| ||_|(_||");
+            Console.SetCursorPosition(13, 8);
+            Console.WriteLine($"1. 플레이어는 타자의 시점에서 게임을 진행한다.");
+            Console.SetCursorPosition(13, 9);
+            Console.WriteLine($"2. 게임이 시작되면 4번의 타석에 설 수 있으며");
+            Console.SetCursorPosition(16, 10);
+            Console.WriteLine($"선택지를 통해 투수의 공을 예상하여 공을");
+            Console.SetCursorPosition(16, 11);
+            Console.WriteLine($"타격하는 방식이다.");
+
+            Console.SetCursorPosition(13, 21);
+            Console.Write("로비로 돌아가시겠습니까? (예: y / 아니오: n): ");
+        }
+        private void MainView()
+        {
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("._____      _        _____._______._____      _      __      __      ");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("|  _  \\    / \\      /     ||  ___||  _  \\    / \\    |  |    |  |     ");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("| |_)  |  / ^ \\    |   (--`| |___ | |_)  |  / ^ \\   |  |    |  |     ");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("|  _  <  / /_\\ \\    \\   \\  |  ___||  _  <  / /_\\ \\  |  |    |  |     ");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("| |_)  |/  ___  \\ --)|   | | |___ | |_)  |/  ___  \\ |  `---.|  `---.");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("|_____//__/   \\__\\|_____/  |_____||_____//__/   \\__\\|______||______|");
+
+            Console.SetCursorPosition(29, 11);
+            Console.WriteLine("- 내일은 타격왕 -");
+
+            Console.SetCursorPosition(31, 15);
+            Console.WriteLine("1. 게임 시작");
+            Console.SetCursorPosition(31, 16);
+            Console.WriteLine("2. 게임 설명");
+            Console.SetCursorPosition(31, 17);
+            Console.WriteLine("3. 게임 종료");
         }
 
     }
